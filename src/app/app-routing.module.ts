12152d20f0164594c7fedb522e0c1d0b67b7bf95
Priscila A. Neves <purishira@gmail.com>
2023-10-31 11:21:07 -0300

    adicionando pokedex

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./pages/pages.module').then(p => p.PagesModule)
+  }
+];
 
+//como essa é a rota principal, deve ser forRoot
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
